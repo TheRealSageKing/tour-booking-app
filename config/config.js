d28cf8dd4b1fccf267ebe7c3ed4f0e4780b54eb1
Hillary Sylvester <insanedude.developer@gmail.com>
2024-04-09 22:02:37 +0100

    Added Session_Secret

--- config/config.js
+++ config/config.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const config = {
 	environment: process.env.APP_ENV || "development",
 	port: process.env.PORT || 5001,
+	session_secret: process.env.SESSION_SECRET || "secret",
 };
 
 module.exports = config;
