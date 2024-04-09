8f8194a8bdb583509c39c54c1f9d75c3c83b699e
Hillary Sylvester <insanedude.developer@gmail.com>
2024-04-09 17:08:33 +0100

    Added Booking Model

---
+++ schemas/BookingSchema.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const BookingSchema = new mongoose.Schema({
+	user: {
+		type: mongoose.SchemaType.ObjectId,
+		ref: "User",
+	},
+	bookingDate: Date,
+	tourType: {
+		type: mongoose.SchemaType.ObjectId,
+		ref: "Tour",
+	},
+	quantity: Number,
+	totalCost: Number,
+	isPending: Boolean,
+	createdAt: {
+		type: Date,
+		default: () => Date.now(),
+		immutable: true,
+	},
+});
+
+module.exports = mongoose.model("Booking", BookingSchema);
