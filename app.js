b676fb6f7da8bd2a7bf96ad559ab04ef2a39b41d
Hillary Sylvester <insanedude.developer@gmail.com>
2024-04-09 12:34:39 +0100

    Adjusted Tailwind to EJS

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const config = require("./config/config");
 
 const app = express();
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/assets"));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
 	res.render("pages/index");
