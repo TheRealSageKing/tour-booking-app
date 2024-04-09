fe4589ce1f26efb1c2d47c7ead05e1bcb6d0c86a
Hillary Sylvester <insanedude.developer@gmail.com>
2024-04-09 21:50:03 +0100

    Add express-session package

--- app.js
+++ app.js
@@ -7,11 +7,24 @@ const AuthRouter = require("./modules/Auth/AuthRoute");
 
 const Database = require("./config/database");
 const UserSchema = require("./schemas/UserSchema");
+const session = require("express-session");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
+app.use(
+	session({
+		secret: config.session_secret,
+		resave: false,
+		saveUninitialized: false,
+		cookie: {
+			secure: config.environment == "production" || false,
+			maxAge: 60000,
+		},
+	})
+);
+
 app.use(express.static(__dirname + "/public"));
 app.use("/assets", [express.static(__dirname + "/node_modules/jquery/dist/")]);
 
